0b4c3c9bf8e701a1dc09242371c8dac666c8f6f9
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-04-30 13:00:34 +0300

    2 задание. Выполнено

--- Exercise02/main.py
+++ Exercise02/main.py
@@ -8,6 +8,10 @@
 значения которых принадлежат заданному диапазону
 (т.е. не меньше заданного минимума и не больше заданного максимума)
 '''
+import random
+
+MIN = -100
+MAX = 100
 
 # Функция вывода автора программы
 def author():
@@ -16,3 +20,26 @@ def author():
     print('Илья "Te3K@_PaynE" Новичихин')
     print('79811131773@yandex.ru')
     print('****************************')
+
+def create_random_array(_num, _min, _max):
+    _list = []
+    for i in range(int(_num)):
+        _list.append(random.randint(_min, _max))
+    return _list
+
+print("Программа создаёт список из случайных чисел")
+print("положительных и отрицательных в диапазоне от -100 до 100")
+print("Затем программа выводит индексы тех чисел, которые лежат")
+print("между минимумом и максимумом, которые вводит пользователь")
+_n = int(input("Введите количество элементов основного массива: "))
+_array_original = create_random_array(_n, MIN, MAX)
+print(_array_original)
+_min = int(input("Введите минимум: "))
+_max = int(input("Введите максимум: "))
+_array_result = list()
+for _i in range(len(_array_original)):
+    if _array_original[_i] >= _min and _array_original[_i] <= _max:
+        _array_result.append(_i)
+print(_array_result)
+
+author()
